5ef2d301fb5bac99ac09e1fe235fb34a581072d9
Miguel Michelson <miguelmichelson@gmail.com>
2025-01-27 00:26:46 -0300

    album metatags

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,6 +4,24 @@ class AlbumsController < ApplicationController
 
   def show
     @release = Release.friendly.find(params[:id])
+    set_meta_tags(
+      title: @release.title,
+      description: "Listen #{@release.title} #{@release.subtitle} on Rauversion",
+      og: {
+        title: @release.title,
+        description: "Listen #{@release.title} #{@release.subtitle} on Rauversion",
+        image: (url_for(@release.cover) rescue nil),
+        type: 'music.album',
+        site_name: 'Rauversion'
+      },
+      twitter: {
+        card: "summary_large_image",
+        site: "@rauversion",
+        title: @release.title,
+        description: "Listen #{@release.title} #{@release.subtitle} on Rauversion",
+        image: (url_for(@release.cover) rescue nil)
+      }
+    )
   end
 
   def index
