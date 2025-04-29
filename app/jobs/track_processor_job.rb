bbf6318864086e35e967aaaeaae09bceb1923e8d
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2025-04-28 22:15:58 -0400

    track processing queue

--- app/jobs/track_processor_job.rb
+++ app/jobs/track_processor_job.rb
@@ -1,5 +1,7 @@
 class TrackProcessorJob < ApplicationJob
-  queue_as :default
+  queue_as :track_processing
+
+  limits_concurrency to: 1, key: ->(arg) { "track_processing" }
 
   def perform(track_id)
     track = Track.find(track_id)
