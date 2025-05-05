16ac33249a82593392e8fce16a8dcc5bd2dc0238
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2025-05-05 01:04:08 -0400

    conversations in service booking

--- app/models/service_booking.rb
+++ app/models/service_booking.rb
@@ -1,4 +1,5 @@
 class ServiceBooking < ApplicationRecord
+  has_many :conversations, as: :messageable, dependent: :destroy
   belongs_to :service_product, class_name: 'Products::ServiceProduct'
   belongs_to :customer, class_name: 'User'
   belongs_to :provider, class_name: 'User'
