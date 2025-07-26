4c50ea1eedc1fda59191092b0d61640111cc7a5e
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2025-07-26 14:34:00 -0400

    remove yjit initializer

--- config/initializers/yjit.rb
+++ config/initializers/yjit.rb
@@ -5,8 +5,8 @@
 # If you are deploying to a memory-constrained environment,
 # you may want to delete this file, but otherwise, it's free
 # performance.
-if defined? RubyVM::YJIT.enable
-  Rails.application.config.after_initialize do
-    RubyVM::YJIT.enable
-  end
-end
+#if defined? RubyVM::YJIT.enable && ENV["RUBY_YJIT_ENABLE"] != "0"
+#  Rails.application.config.after_initialize do
+#    RubyVM::YJIT.enable
+#  end
+#end
