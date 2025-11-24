79ab0632c4ef41ae6db0f3017bea4cf2de81d811
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2025-11-23 23:19:21 -0300

    fix spec

--- spec/models/event_host_spec.rb
+++ spec/models/event_host_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe EventHost, type: :model do
   describe "associations" do
     it { should belong_to(:event) }
-    it { should belong_to(:user) }
+    it { should belong_to(:user).optional }
   end
 
   describe "#invite_user" do
